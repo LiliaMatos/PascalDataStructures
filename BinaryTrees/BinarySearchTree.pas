a455423b26e260c96c9d5ce3cc7b85cece211716
grun00 <lucas.c.grunheidt@gmail.com>
2019-11-01 14:47:30 -0300

    BST: Count nodes, even nodes and odd nodes

--- BinaryTrees/BinarySearchTree.pas
+++ BinaryTrees/BinarySearchTree.pas
@@ -34,4 +34,132 @@ begin
     end;
 end;
 
-  
+function CountNodes(node:BSTPointer):integer;
+begin
+  if not(node) then
+    begin
+      CountNodes := 0;
+    end
+  else if not(node^.Left) and not(node^.Right) then
+    begin
+      CountNodes := 1;
+    end
+  else if not(node^.Left) then
+    begin
+      CountNodes := CountNodes(node^.Right) + 1;
+    end
+  else if not(node^.Right) then
+    begin
+      CountNodes := CountNodes(node^.Left) + 1; 
+    end
+  else
+    begin
+      CountNodes := CountNodes(node^.Left) + CountNodes(node^.Right) + 1; 
+    end; 
+end;
+
+function CountEvenNodes(node:BSTPointer):integer;
+begin
+  if not(node) then
+    begin
+      CountEvenNodes := 0;
+    end
+  else if not(node^.Left) and not(node^.Right) then
+    begin
+      if (node^.Info mod 2 = 0) then
+        begin
+          CountEvenNodes := 1;
+        end
+      else
+        begin
+          CountEvenNodes := 0; 
+        end;
+    end
+  else if not(node^.Left) then
+    begin
+      if (node^.Info mod 2 = 0) then
+        begin
+          CountEvenNodes := CountEvenNodes(node^.Right) + 1;
+        end
+      else
+        begin
+          CountEvenNodes := CountEvenNodes(node^.Right); 
+        end;
+    end
+  else if not(node^.Right) then
+    begin
+      if (node^.Info mod 2 = 0) then
+        begin
+          CountEvenNodes := CountEvenNodes(node^.Left) + 1;
+        end
+      else
+        begin
+          CountEvenNodes := CountEvenNodes(node^.Left); 
+        end;
+    end
+  else
+    begin
+      if (node^.Info mod 2 = 0) then
+        begin
+          CountEvenNodes := CountEvenNodes(node^.Left) + CountEvenNodes(node^.Right) + 1; 
+        end
+      else 
+        begin
+          CountEvenNodes := CountEvenNodes(node^.Left) + CountEvenNodes(node^.Right); 
+        end;
+    end; 
+end;
+
+function CountOddNodes(node:BSTPointer):integer;
+begin
+  if not(node) then
+    begin
+      CountOddNodes := 0;
+    end
+  else if not(node^.Left) and not(node^.Right) then
+    begin
+      if (node^.Info mod 2 <> 0) then
+        begin
+          CountOddNodes := 1;
+        end
+      else
+        begin
+          CountOddNodes := 0; 
+        end;
+    end
+  else if not(node^.Left) then
+    begin
+      if (node^.Info mod 2 <> 0) then
+        begin
+          CountOddNodes := CountOddNodes(node^.Right) + 1;
+        end
+      else
+        begin
+          CountOddNodes := CountOddNodes(node^.Right); 
+        end;
+    end
+  else if not(node^.Right) then
+    begin
+      if (node^.Info mod 2 <> 0) then
+        begin
+          CountOddNodes := CountOddNodes(node^.Left) + 1;
+        end
+      else
+        begin
+          CountOddNodes := CountOddNodes(node^.Left); 
+        end;
+    end
+  else
+    begin
+      if (node^.Info mod 2 <> 0) then
+        begin
+          CountOddNodes := CountOddNodes(node^.Left) + CountOddNodes(node^.Right) + 1; 
+        end
+      else 
+        begin
+          CountOddNodes := CountOddNodes(node^.Left) + CountOddNodes(node^.Right); 
+        end;
+    end; 
+end;
+
+function BinarySearch(value:integer; node: BSTPointer):boolean;
