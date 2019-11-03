5cfa9a94e87f4287223be4a6f93c887379420d60
grun00 <lucas.c.grunheidt@gmail.com>
2019-11-03 13:41:30 -0300

    Added Print Function and AddToEnd

--- LinkedLists/Double/linked_list_double.pas
+++ LinkedLists/Double/linked_list_double.pas
@@ -10,6 +10,7 @@ type
   end;
 
   {*Init, PrintAllForwards, PrintAllBackWards, AddToStart, AddToEnd, AddToXForward, AddToXBackward, AddBetween, RemoveStart, RemoveEnd, RemoveX*}
+
 function Init(value:integer; node: NodePointer):NodePointer;
 begin
   new(node);
@@ -19,10 +20,48 @@ begin
   Init := node; 
 end;
 
+procedure AddToEnd(value:integer; head:NodePointer);
+var
+  nav, aux: NodePointer;
+begin
+  nav := head;
+  while(nav^.Next <> nil) do
+    begin
+      nav := nav^.Next;
+    end;
+  new(aux);
+  aux := Init(value, aux);
+  aux^.Prev := nav;
+  nav^.Next := aux;
+end;
+
+procedure PrintAllForwards(head:NodePointer);
+var
+  nav: NodePointer;
+begin
+  nav := head;
+  write('List: ');
+  while(nav^.Next <> nil) do
+    begin
+      write(nav^.Info, ', ' );
+      nav := nav^.Next;
+    end;
+  writeln(nav^.Info);
+
+end;
+
 var
   head: NodePointer;
   index: integer;
 begin
+  writeln('Init(1, head): ');
+  new(head);
   head := Init(1, head);
-  writeln(head^.Info);
+  writeln('Info: ', head^.Info);
+
+  writeln('AddToEnd(2-10, head)');
+  for index := 2 to 10 do 
+    AddToEnd(index, head); 
+  PrintAllForwards(head);
 end.
+
